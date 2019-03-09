db2c65722549c4bbd155f25820d5986441097f56
nitanmarcel <nitan.marcel@gmail.com>
2019-03-09 13:23:12 +0200

    Update notes to save media and document files.

--- tg_companion/modules/global_notes.py
+++ tg_companion/modules/global_notes.py
@@ -1,31 +1,23 @@
+from io import BytesIO
+
 import sqlalchemy as db
+from telethon.tl.types import (DocumentAttributeFilename,
+                               InputMediaUploadedDocument, MessageMediaPhoto)
 
 from tg_companion.tgclient import DB_URI, client
-
-NOTES = {}
 
 engine = db.create_engine(DB_URI)
 metadata = db.MetaData()
 
-notes_tbl = db.Table("notes", metadata,
-                     db.Column("notename", db.String(), primary_key=True),
-                     db.Column("note", db.String()))
-
+notes_tbl = db.Table("global_notes", metadata,
+                     db.Column("notename", db.String()),
+                     db.Column("notetext", db.String()),
+                     db.Column("file", db.LargeBinary()),
+                     db.Column("filetype", db.String()),
+                     db.Column("filename", db.String()),
+                     db.Column("mimetype", db.String()))
 
 metadata.create_all(engine, [notes_tbl], checkfirst=True)
-
-
-def _load_notes():
-    NOTES.clear()
-    query = db.select([notes_tbl])
-    connection = engine.connect()
-    for row in connection.execute(query).fetchall():
-        if row:
-            NOTES.update({row[0]: row[1]})
-    connection.close()
-
-
-_load_notes()
 
 SAVE_HELP = """
     **Globally save a note.**
@@ -53,58 +45,114 @@ NOTES_HELP = """
 @client.CommandHandler(outgoing=True, command="save", help=SAVE_HELP)
 @client.log_exception
 async def save_note(event):
+    message = event.message
     split_text = event.text.split(None, 2)
 
     if event.reply_to_msg_id:
-        repl_msg = await event.get_reply_message()
+        message = await event.get_reply_message()
         if len(split_text) == 1:
             await client.update_message(event, SAVE_HELP)
             return
         note_name = split_text[1]
-        note_content = repl_msg.text
-        if not note_content:
-            await client.update_message(event, "`Unsupported note content! Please make sure you reply to a text message!!`")
-            return
+        note_text = message.text
     else:
         if len(split_text) <= 2:
             await client.update_message(event, SAVE_HELP)
             return
+        note_text = split_text[2]
 
-        note_name = split_text[1]
-        note_content = split_text[2]
+    note_name = split_text[1]
+    msg_file = None
+    file_type = None
+    file_name = None
+    mime_type = None
+    if message.media or message.document:
+        msg_file = message.document or message.media
+        if message.media and not message.document:
+            if isinstance(message.media, MessageMediaPhoto):
+                file_type = "media"
+                file_name = "unknown.png"
+                mime_type = "image/png"
+            else:
+                await event.reply("Unsuported Media Type")
+        if message.document:
+            file_type = "document"
+            mime_type = message.document.mime_type
+            for attribute in message.document.attributes:
+                if isinstance(attribute, DocumentAttributeFilename):
+                    file_name = attribute.file_name
 
-    connection = engine.connect()
-
-    if note_name not in NOTES:
-        query = db.insert(notes_tbl).values(
-            notename=note_name, note=note_content)
-    else:
-        query = db.update(notes_tbl).where(
-            notes_tbl.columns.notename == note_name).values(
-            note=note_content)
-    connection.execute(query)
-    connection.close()
-
+    if msg_file:
+        msg_file = await client.download_file(msg_file)
+        msg_file = msg_file
+    with engine.connect() as conn:
+        query = (
+            db.select(
+                [notes_tbl]).where(
+                notes_tbl.columns.notename == note_name))
+        if conn.execute(query).fetchall():
+            query = (
+                db.update(notes_tbl).where(
+                    notes_tbl.columns.notename == note_name).values(
+                    notename=note_name,
+                    notetext=note_text,
+                    file=msg_file,
+                    filetype=file_type,
+                    filename=file_name,
+                    mimetype=mime_type,
+                ))
+        else:
+            query = (
+                db.insert(notes_tbl).values(
+                    notename=note_name,
+                    notetext=note_text,
+                    file=msg_file,
+                    filetype=file_type,
+                    filename=file_name,
+                    mimetype=mime_type))
+        conn.execute(query)
     await client.update_message(event, f"Globally saved `{note_name}`. Get it using the command `get {note_name}`")
-    _load_notes()
 
 
 @client.CommandHandler(outgoing=True, command="get", help=GET_HELP)
 @client.log_exception
 async def get_note(event):
     split_text = event.text.split(None, 1)
+    chat = await event.get_chat()
 
     if len(split_text) == 1:
         await client.update_message(event, GET_HELP)
         return
 
     note_name = split_text[1]
+    note_text = None
+    msg_file = None
+    file_type = None
+    file_name = None
+    mime_type = None
 
-    if note_name not in NOTES:
-        await client.update_message(event, "There's no note with that name")
-        return
-
-    await client.update_message(event, NOTES.get(note_name))
+    with engine.connect() as conn:
+        query = (
+            db.select(
+                [notes_tbl]).where(
+                notes_tbl.columns.notename == note_name))
+        for row in conn.execute(query).fetchall():
+            note_text = row.notetext
+            msg_file = row.file
+            file_type = row.filetype
+            file_name = row.filename
+            mime_type = row.mimetype
+    if msg_file:
+        with BytesIO(msg_file) as mem_file:
+            uploaded_file = await client.upload_file(mem_file.read(), file_name=file_name)
+            if file_type == "document":
+                await client.send_file(chat.id, InputMediaUploadedDocument(file=uploaded_file, mime_type=mime_type, attributes=[DocumentAttributeFilename(file_name)]), caption=note_text, force_document=True)
+            else:
+                mem_file.name = "photo.png"
+                file_name = "photo.png"
+                await client.send_file(chat.id, uploaded_file, caption=note_text)
+            return
+    await client.update_message(event, note_text)
 
 
 @client.CommandHandler(outgoing=True, command="remove", help=REMOVE_HELP)
@@ -114,18 +162,18 @@ async def remove_note(event):
     if len(split_text) == 1:
         await client.update_message(event, REMOVE_HELP)
         return
-
     note_name = split_text[1]
 
-    if note_name not in NOTES:
-        await client.update_message(event, "There's no note with that name")
-        return
-
-    query = notes_tbl.delete().where(notes_tbl.columns.notename == note_name)
-    connection = engine.connect()
-    connection.execute(query)
-    connection.close()
-    _load_notes()
+    query = (
+        db.select(
+            [notes_tbl]).where(
+            notes_tbl.columns.notename == note_name))
+    with engine.connect() as conn:
+        if not conn.execute(query).fetchall():
+            await client.update_message(event, "`There's no note with that name`")
+            return
+        query = notes_tbl.delete().where(notes_tbl.columns.notename == note_name)
+        conn.execute(query)
     await client.update_message(event, f"Deleted `{note_name}` from database")
 
 
@@ -133,7 +181,11 @@ async def remove_note(event):
 @client.log_exception
 async def list_notes(event):
     listnotes = []
-    for notename, _ in NOTES.items():
-        listnotes.append(notename)
-    await client.update_message(event, "`Globally saved notes:`\n-" +
-                                "\n-".join(listnotes))
+    with engine.connect() as conn:
+        query = (db.select([notes_tbl]))
+        for row in conn.execute(query).fetchall():
+            listnotes.append(row.notename)
+    if not listnotes:
+        await client.update_message(event, "`There are no notes saved`")
+        return
+    await client.update_message(event, "`Globally saved notes:`\n-" + "\n-".join(listnotes))
